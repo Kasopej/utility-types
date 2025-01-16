43df58bdcd9a27236c09a2892b34eb88f46aa72f
Kasopej <87914956+Kasopej@users.noreply.github.com>
2025-01-16 11:07:13 +0100

    Update redux-action-infer.ts

--- redux-action-infer.ts
+++ redux-action-infer.ts
@@ -1,3 +1,5 @@
 type SliceActions<T> = {
   [K in keyof T]: T[K] extends (...args: any[]) => infer A ? A : never;
 }[keyof T];
+type ActionTypes = SliceActions<typeof cartStore.actions>;
+type AppDispatch = typeof store.dispatch<ActionTypes>;
